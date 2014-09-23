4d4bea027db99fd4660e58de8318345b23b5b299
Brian Clements <brian@brianclements.net>
2014-09-22 21:22:35 -0700

    Layout tweaks, fix DBDATA confusion.
    DB_DIR is for actual db data, DBDATA is for configuration location.

--- spoke/entrypoint.sh
+++ spoke/entrypoint.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 set -e
 
+# Tunable settings
 DB_USER=${DB_USER:-tempuser}
 DB_PASS=${DB_PASS:-`date | md5sum | head -c10`}
 DB_NAME=${DB_NAME:-tempdb}
+DB_DIR=${DB_DIR:-/data/dbdata}
 SETUP_DB_COMMANDS=${SETUP_DB_COMMANDS:-/config/postgresql/setupdb}
+
+# Misc settings
+CONF=/config/postgresql/postgresql.conf
 PG_BIN=/usr/lib/postgresql/9.3/bin
+PG_TERM=/usr/bin/psql
 INIT_DB=$PG_BIN/initdb
 PG_CMD=$PG_BIN/postgres
-PG_TERM=/usr/bin/psql
-CONF=/config/postgresql/postgresql.conf
-ERR_LOG=${ERR_LOG:-"/log/$HOSTNAME/postgresql_stderr.log"}
-# postgres reads this implicitly
-export PGDATA=${PGDATA:-/data/dbdata}
+ERR_LOG=/log/$HOSTNAME/postgresql_stderr.log
+
+# Postgres reads this implicitly. Location of configuration files.
+export PGDATA=/config/postgresql
 
 postgres_run() {
     su postgres sh -c "$@"
 }
 
 # create dirs if needed
-mkdir -p $PGDATA
+mkdir -p $DB_DIR
 postgres_run "mkdir -p -m 775 /var/run/postgresql"
 
 # initialize db if needed
-if [ ! "`ls -A $PGDATA`" ] ; then
-    chmod 700 $PGDATA
-    chown -R postgres $PGDATA
-    postgres_run "$INIT_DB $PGDATA" | tee -a $ERR_LOG
+if [ ! "`ls -A $DB_DIR`" ] ; then
+    chmod 700 $DB_DIR
+    chown -R postgres $DB_DIR
+    postgres_run "$INIT_DB $DB_DIR" | tee -a $ERR_LOG
 
     # first-run db and owner setup
     postgres_run "$PG_BIN/pg_ctl start"
@@ -42,8 +47,8 @@ if [ ! "`ls -A $PGDATA`" ] ; then
     sleep 2s
 
     # Info
-    echo -e "Starting Postgres...\nInfo:\n  Username: $DB_USER\n  Password: $DB_PASS\n  Database: $DB_NAME" | tee -a $ERR_LOG
+    echo -e "Starting Postgres...\nInfo:\n  Username: $DB_USER\n  Password: $DB_PASS\n  Database: $DB_NAME\n  Location: $DB_DIR" | tee -a $ERR_LOG
 fi
 
 # Run
-exec su postgres sh -c "$PG_CMD -c config_file=$CONF"
+exec su postgres sh -c "$PG_CMD"
